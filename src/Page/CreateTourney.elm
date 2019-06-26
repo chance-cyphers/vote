61ddcaf7f770629e69fbae531b2c4f81d41efe26
chance.a.cyphers <chance.a.cyphers@gmail.com>
2019-06-26 12:51:41 -0400

    Maintain character order when creating

--- src/Page/CreateTourney.elm
+++ src/Page/CreateTourney.elm
@@ -6,16 +6,19 @@ import Html.Attributes exposing (class, placeholder, step, type_, value)
 import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Encode as Encode
-import List exposing (map)
 import String exposing (toInt)
-type alias Model =
-    { title: String
-    , matchDuration: Int
-    , characters: Dict String String
-    }
+
+
 
 
 -- INIT
+
+type alias Model =
+    { title: String
+    , matchDuration: Int
+    , characters: Dict Int String
+    }
+
 
 init: (Model, Cmd Msg)
 init =
@@ -27,14 +30,13 @@ init =
     )
 
 
-
 -- UPDATE
 
 type Msg
   = Hi
   | Title String
   | MatchDuration String
-  | Character String String
+  | Character Int String
   | Submit
   | CreatedTourney (Result Http.Error String)
 
@@ -95,31 +97,32 @@ view model =
       [ h1 [] [ text "Create page" ]
       , input [ type_ "text", placeholder "Title", value model.title, onInput Title] []
       , input [ type_ "number", step "1", placeholder "Match Duration", value (Debug.toString model.matchDuration), onInput MatchDuration] []
-      , characterInput "c1" model.characters
-      , characterInput "c2" model.characters
-      , characterInput "c3" model.characters
-      , characterInput "c4" model.characters
-      , characterInput "c5" model.characters
-      , characterInput "c6" model.characters
-      , characterInput "c7" model.characters
-      , characterInput "c8" model.characters
-      , characterInput "c9" model.characters
-      , characterInput "c10" model.characters
-      , characterInput "c11" model.characters
-      , characterInput "c12" model.characters
-      , characterInput "c13" model.characters
-      , characterInput "c14" model.characters
-      , characterInput "c15" model.characters
-      , characterInput "c16" model.characters
+      , characterInput 1 model.characters
+      , characterInput 2 model.characters
+      , characterInput 3 model.characters
+      , characterInput 4 model.characters
+      , characterInput 5 model.characters
+      , characterInput 6 model.characters
+      , characterInput 7 model.characters
+      , characterInput 8 model.characters
+      , characterInput 9 model.characters
+      , characterInput 10 model.characters
+      , characterInput 11 model.characters
+      , characterInput 12 model.characters
+      , characterInput 13 model.characters
+      , characterInput 14 model.characters
+      , characterInput 15 model.characters
+      , characterInput 16 model.characters
       , button [ onClick Submit ] [ text "Create" ]
       ]
 
 
+characterInput: Int -> Dict Int String -> Html Msg
 characterInput pos characters =
   input [ type_ "text", placeholder "character", value (getCharacter pos characters), onInput (Character pos)] []
 
 
-getCharacter: String -> Dict String String -> String
+getCharacter: Int -> Dict Int String -> String
 getCharacter pos dict =
   let
     maybeChar = get pos dict
